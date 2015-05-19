9eabf2e5ad0cd5fb0a2609b51e0b57db40ed64f0
Doug Beare <doug.beare@gmail.com>
2015-05-19 16:44:10 +0100

    Update createDB.sh

--- createDB.sh
+++ createDB.sh
@@ -8,7 +8,7 @@ psql -d effdis
 CREATE EXTENSION postgis;
 CREATE EXTENSION postgis_topology;
 
-psql -U postgres -d effdis -f /usr/share/postgresql/9.4/contrib/postgis-2.0/postgis.sql
+psql -U postgres -d effdis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis.sql
 psql -U postgres -d effdis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/spatial_ref_sys.sql
 psql -U postgres -d effdis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis_comments.sql
 
