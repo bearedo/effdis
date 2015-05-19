09329585fdbf670cf6ab2b7683bbe671f3b196fc
Doug Beare <doug.beare@gmail.com>
2015-05-19 17:14:22 +0100

    Update createPermissions.sh

--- createPermissions.sh
+++ createPermissions.sh
@@ -1,7 +1,13 @@
-psql -U postgres -d effdis -c "CREATE ROLE dbeare WITH PASSWORD 'cum2brae';"
+psql -U postgres -d effdis -c "CREATE USER dbeare WITH PASSWORD 'cum2brae';"
 psql -U postgres -d effdis -c "GRANT ALL PRIVILEGES ON DATABASE effdis to dbeare;"
+psql -U postgres -d effdis -c "GRANT USAGE ON SCHEMA public TO dbeare;"
+psql -U postgres -d effdis -c "select 'grant select on '||schemaname||'.'||tablename||' to dbeare;' from pg_tables where schemaname in ('public');"
+psql -U postgres -d effdis -c "select 'grant select on '||schemaname||'."'||viewname||'"to dbeare;' from pg_views where schemaname in ('public');"
 
-psql -U postgres -d effdis -c "CREATE ROLE lkell WITH PASSWORD 'Lkell$2o13';"
+
+
+
+psql -U postgres -d effdis -c "CREATE USER lkell WITH PASSWORD 'Lkell$2o13';"
 psql -U postgres -d effdis -c "GRANT ALL PRIVILEGES ON DATABASE effdis to lkell;"
 
 
