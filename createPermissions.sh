437b93c28c504c7d8786335cf3fac68f98180d2a
Doug Beare <doug.beare@gmail.com>
2015-05-22 15:56:36 +0100

    Update createPermissions.sh

--- createPermissions.sh
+++ createPermissions.sh
@@ -1,13 +1,16 @@
-psql -U postgres -d effdis -c "CREATE USER dbeare WITH PASSWORD 'cum2brae';"
-psql -U postgres -d effdis -c "GRANT ALL PRIVILEGES ON DATABASE effdis to dbeare;"
-psql -U postgres -d effdis -c "GRANT USAGE ON SCHEMA public TO dbeare;"
-psql -U postgres -d effdis -c "select 'grant select on '||schemaname||'.'||tablename||' to dbeare;' from pg_tables where schemaname in ('public');"
-psql -U postgres -d effdis -c "select 'grant select on '||schemaname||'."'||viewname||'"to dbeare;' from pg_views where schemaname in ('public');"
+
+su - postgres
+
+
+psql  -d effdis -c "GRANT ALL PRIVILEGES ON DATABASE effdis to dbeare;"
+psql  -d effdis -c "GRANT USAGE ON SCHEMA public TO dbeare;"
+psql -d effdis -c "select 'grant select on '||schemaname||'.'||tablename||' to dbeare;' from pg_tables where schemaname in ('public');"
+psql -d effdis -c "select 'grant select on '||schemaname||'."'||viewname||'"to dbeare;' from pg_views where schemaname in ('public');"
 
 
 
 
-psql -U postgres -d effdis -c "CREATE USER lkell WITH PASSWORD 'Lkell$2o13';"
-psql -U postgres -d effdis -c "GRANT ALL PRIVILEGES ON DATABASE effdis to lkell;"
+psql -d effdis -c "CREATE USER lkell WITH PASSWORD 'Lkell$2o13';"
+psql -d effdis -c "GRANT ALL PRIVILEGES ON DATABASE effdis to lkell;"
 
 
