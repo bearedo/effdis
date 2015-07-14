097899fa513b7cede83fc5f7644f801d9db45610
bearedo <doug.beare@gmail.com>
2015-07-14 14:37:53 +0100

    Changing case for use with database

--- R/yr.month.coverage.task2.r
+++ R/yr.month.coverage.task2.r
@@ -1,23 +1,27 @@
-yr.month.coverage.task2.r<-function(tdata=t2ceLL,which.region = 'AT',which.flag='EU.Portugal'){
 
-#3D plot to explore temporal confounding
- #tdata <- t2ceLL
- #which.region <- 'AT'
- #which.flag   <- 'EU.Portugal'
- fdata <- tdata[tdata$Region == which.region & tdata$FlagName == which.flag,]
 
-ymc <- table(fdata$YearC,fdata$TimePeriodID) # Number of observations by year and month
-dimnames(ymc)[[1]] <- c(min(fdata$YearC,na.rm=T):max(fdata$YearC,na.rm=T)) 
-dimnames(ymc)[[2]] <- month.abb
-
-image(min(fdata$YearC,na.rm=T):max(fdata$YearC,na.rm=T),1:12,ymc,xaxt='n',yaxt='n',xlab="",ylab="",col=terrain.colors(100),
-      xlim=range(tdata$YearC,na.rm=T),ylim=range(tdata$TimePeriodID,na.rm=T))
-contour(min(fdata$YearC,na.rm=T):max(fdata$YearC,na.rm=T),1:12,ymc,add=T)
-
-axis(side=1,at=min(tdata$YearC,na.rm=T):max(tdata$YearC,na.rm=T),label=sort(unique(tdata$YearC)))
-ms <- range(fdata$TimePeriodID,na.rm=T)
-axis(side=2,at=ms[1]:ms[2],label=month.abb[ms[1]:ms[2]])
-title(which.flag)
-
+yr.month.coverage.task2.r<-function(tdata=t2ce,which.gear='LL',which.flag='EU.Portugal'){
+  
+  #3D plot to explore temporal confounding
+  #tdata <- t2ce
+  #which.flag   <- 'EU.Portugal'
+  fdata <- tdata[tdata$flagname == which.flag & tdata$geargrpcode == which.gear & fdata$timeperiodid < 13,]
+  fdata$trend <- trend.r(year=fdata$yearc,month=fdata$timeperiodid,start.year=1950)
+  
+  
+  par(mfrow=c(1,1))
+  ymc <- table(fdata$yearc,fdata$timeperiodid) # Number of observations by year and month
+  dimnames(ymc)[[1]] <- sort(unique(fdata$yearc))
+  dimnames(ymc)[[2]] <- month.abb
+  
+  image(as.numeric(sort(unique(row.names(ymc)))),1:12,ymc,xaxt='n',yaxt='n',xlab="",ylab="",col=terrain.colors(100),
+        xlim=range(tdata$yearc,na.rm=T),ylim=range(fdata$timeperiodid,na.rm=T))
+  contour(as.numeric(sort(unique(row.names(ymc)))),1:12,ymc,add=T)
+  
+  axis(side=1,at=min(tdata$yearc,na.rm=T):max(tdata$yearc,na.rm=T),label=sort(unique(tdata$yearc)))
+  ms <- range(fdata$timeperiodid,na.rm=T)
+  axis(side=2,at=ms[1]:ms[2],label=month.abb[ms[1]:ms[2]])
+  title(paste(which.flag,which.gear))
+  
 }
 
