5a8772219125eb9df122fae59e8fb8dc284fbba1
bearedo <doug.beare@gmail.com>
2015-07-15 09:27:46 +0100

    Various refinements.

--- R/spatial.coverage.by.year.task2.r
+++ R/spatial.coverage.by.year.task2.r
@@ -1,18 +1,19 @@
-spatial.coverage.by.year.task2.r <- function(tdata=t2ce,which.gear='LL',which.flag='EU.Portugal'){
-  #tdata <- t2ce
-  #which.gear <- 'LL'
-  #which.flag   <- 'EU.Portugal'
+
+spatial.coverage.by.year.task2.r <- function(tdata=t2ce,start.year=1950, end.year=2015, which.gear='LL',which.flag='EU.Portugal'){
+ # tdata <- t2ce
+ # which.gear <- 'LL'
+ # which.flag   <- 'Chinese Taipei'
   
   fdata <- tdata[tdata$flagname == which.flag & tdata$geargrpcode == which.gear,]
   
-  ys <- sort(unique(tdata$yearc))
+  ys <- start.year:end.year
   ly <- length(ys) # 43 years
-  par(mfrow=c(7,9),mar=c(0,0,1,0))
+  par(mfrow=c(6,11),mar=c(0,0,1,0))
   for (i in min(ys,na.rm=T):max(ys,na.rm=T))
   {
     dat <- fdata[fdata$yearc == i,]
     if(length(dat[,1])==0){
-      plot(dat$longitude,dat$latitude,type='n',xaxt='n',yaxt='n',ylim=range(tdata$latitude,na.rm=T),xlim=range(tdata$longitude,na.rm=T))
+      plot(fdata$longitude,fdata$latitude,type='n',xaxt='n',yaxt='n',ylim=range(tdata$latitude,na.rm=T),xlim=range(tdata$longitude,na.rm=T))
       map('world',col='green',fill=T,add=T)
       title(i,cex.main=.75)}
     else{
@@ -21,7 +22,7 @@ spatial.coverage.by.year.task2.r <- function(tdata=t2ce,which.gear='LL',which.fl
       map('world',add=T,col='green',fill=T)
       title(i,cex.main=.75)
     }
-    mtext(side=3,outer=T,paste(which.flag,which.gear))  
+    mtext(side=3,outer=T,paste(which.flag,which.gear, sep=' - '))  
 
   }
 }
--- R/yr.month.coverage.task2.r
+++ R/yr.month.coverage.task2.r
@@ -1,27 +1,40 @@
+   
 
-
-yr.month.coverage.task2.r<-function(tdata=t2ce,which.gear='LL',which.flag='EU.Portugal'){
+yr.month.coverage.task2.r<-function(tdata=t2ce,start.year=1950,end.year=2015,which.gear='LL',which.flag='EU.Portugal'){
   
   #3D plot to explore temporal confounding
-  #tdata <- t2ce
-  #which.flag   <- 'EU.Portugal'
-  fdata <- tdata[tdata$flagname == which.flag & tdata$geargrpcode == which.gear & fdata$timeperiodid < 13,]
-  fdata$trend <- trend.r(year=fdata$yearc,month=fdata$timeperiodid,start.year=1950)
+  #tdata <- t2ce; which.flag   <- 'U.S.A.'; which.gear <- 'LL'
+  tdata1 <- tdata[tdata$timeperiodid < 13,]
+  fdata <- tdata1[tdata1$flagname == which.flag & tdata1$geargrpcode == which.gear,]
   
+  dd <- dim(fdata)
+  print(dd)
+  if(dd[1] == 0)
+  {
+    print('There are no data')
+    }
+  
+  else{
   
   par(mfrow=c(1,1))
-  ymc <- table(fdata$yearc,fdata$timeperiodid) # Number of observations by year and month
+  
+    fmat <- matrix(NA, length(start.year:end.year),12)
+    dimnames(fmat) <- list(c(start.year:end.year),1:12)
+    
+    ymc <- table(fdata$yearc,fdata$timeperiodid) # Number of observations by year and month
   dimnames(ymc)[[1]] <- sort(unique(fdata$yearc))
   dimnames(ymc)[[2]] <- month.abb
+  mm <- match(dimnames(ymc)[[1]],dimnames(fmat)[[1]])
+  fmat[mm,] <- ymc
   
-  image(as.numeric(sort(unique(row.names(ymc)))),1:12,ymc,xaxt='n',yaxt='n',xlab="",ylab="",col=terrain.colors(100),
-        xlim=range(tdata$yearc,na.rm=T),ylim=range(fdata$timeperiodid,na.rm=T))
-  contour(as.numeric(sort(unique(row.names(ymc)))),1:12,ymc,add=T)
+  image(start.year:end.year,1:12,fmat,xaxt='n',yaxt='n',xlab="",ylab="",col=terrain.colors(100),
+        xlim=c(start.year,end.year),ylim=range(fdata$timeperiodid,na.rm=T))
+  contour(start.year:end.year,1:12,fmat,add=T)
   
-  axis(side=1,at=min(tdata$yearc,na.rm=T):max(tdata$yearc,na.rm=T),label=sort(unique(tdata$yearc)))
+  axis(side=1,at=start.year:end.year,label=as.character(start.year:end.year))
   ms <- range(fdata$timeperiodid,na.rm=T)
   axis(side=2,at=ms[1]:ms[2],label=month.abb[ms[1]:ms[2]])
-  title(paste(which.flag,which.gear))
-  
+  title(paste(which.flag,which.gear, sep= ' - '))
+  }
 }
 
