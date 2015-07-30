4dbb3c10c904c7f8903d7270732d87e0b93b3a1e
Doug Beare <doug.beare@gmail.com>
2015-07-30 09:48:54 +0100

    changed yearc to year

--- R/spatial.coverage.by.year.task2.r
+++ R/spatial.coverage.by.year.task2.r
@@ -1,17 +1,17 @@
 
 spatial.coverage.by.year.task2.r <- function(tdata=t2ce,start.year=1950, end.year=2010, which.region = 'AT', which.gear='LL',which.flag='EU.Portugal'){
-  #tdata <- t2ce
+  #tdata <- out
   #which.gear <- 'LL'
-  #which.flag   <- 'Chinese Taipei'
-  
-  fdata <- tdata[tdata$flagname == which.flag & tdata$geargrpcode == which.gear & tdata$region == which.region,]
+  #which.flag   <- 'Brasil'
+  #which.region <- 'AT';start.year <- 1990; end.year <- 1995;
+  #fdata <- tdata[tdata$flagname == which.flag & tdata$geargrpcode == which.gear & tdata$region == which.region,]
   
   ys <- start.year:end.year
   ly <- length(ys) # 43 years
   #par(mfrow=c(9,7),mar=c(0,0,1,0))
   for (i in min(ys,na.rm=T):max(ys,na.rm=T))
   {
-    dat <- fdata[fdata$yearc == i,]
+    dat <- fdata[fdata$year == i,]
     #print(dim(dat))
     if(length(dat[,1])==0){
       plot(fdata$longitude,fdata$latitude,type='n',xaxt='n',yaxt='n',ylim=range(tdata$latitude,na.rm=T),xlim=range(tdata$longitude,na.rm=T))
@@ -19,7 +19,7 @@ spatial.coverage.by.year.task2.r <- function(tdata=t2ce,start.year=1950, end.yea
       title(i,cex.main=1)}
     else{
       plot(dat$longitude,dat$latitude,type='n',xaxt='n',yaxt='n',ylim=range(tdata$latitude,na.rm=T),xlim=range(tdata$longitude,na.rm=T))
-      points(dat$longitude,dat$latitude,pch='.',col='red')
+      points(dat$longitude,dat$latitude,pch=16,col='red')
       map('world',add=T,col='green',fill=T)
       title(i,cex.main=1)
     }
