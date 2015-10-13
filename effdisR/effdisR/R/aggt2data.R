0b603d6f1df164268695d40c1377611038323689
bearedo <doug.beare@gmail.com>
2015-10-13 08:24:56 +0100

    Changed what.flag to which.flag

--- effdisR/effdisR/R/aggt2data.R
+++ effdisR/effdisR/R/aggt2data.R
@@ -1,14 +1,14 @@
 aggt2data <-
-function(input=lllf, whichflag ='Japan',which.effort='NO.HOOKS')
+function(input=lllf, which.flag ='Japan',which.effort='NO.HOOKS')
 
   {
   
-  input <- lllf;what.flag='Japan';what.effort='NO.HOOKS'
+  #input <- lllf;what.flag='Japan';what.effort='NO.HOOKS'
   
   if(which.flag=='All')
   {
     
-    input_kgs <- input[input$catchunit == 'kg' & input$eff1type == what.effort,]
+    input_kgs <- input[input$catchunit == 'kg' & input$eff1type == which.effort,]
     allt2 <- aggregate(list(eff=input_kgs$eff1,measured_catch=input_kgs$measured_catch), 
                        by=list(year=input_kgs$year,month=input_kgs$month,longitude=input_kgs$longitude,latitude=input_kgs$latitude,
                                species=input_kgs$species,flagname=input_kgs$flagname),sum,na.rm=T)
@@ -17,7 +17,7 @@ function(input=lllf, whichflag ='Japan',which.effort='NO.HOOKS')
   }
   
   else{
-  input_kgs <- input[input$catchunit == 'kg' & input$eff1type == what.effort & lllf$flagname == what.flag,]
+  input_kgs <- input[input$catchunit == 'kg' & input$eff1type == which.effort & lllf$flagname == which.flag,]
 
 allt2 <- aggregate(list(eff=input_kgs$eff1,measured_catch=input_kgs$measured_catch), 
                    by=list(year=input_kgs$year,month=input_kgs$month,longitude=input_kgs$longitude,latitude=input_kgs$latitude,species=input_kgs$species,
