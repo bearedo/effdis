e90901ea4f29f8cbf4c97514e9392cb7fff53600
bearedo <doug.beare@gmail.com>
2015-11-16 17:24:02 +0000

    Updated input table

--- R/old/get.effdis.t1.data.r
+++ R/old/get.effdis.t1.data.r
@@ -15,7 +15,7 @@ get.effdis.t1.data <- function(which.dsn="effdis-tuna-cc1",which.gear='LL',which
 if(which.flag=='All') # If which.flag is 'All' extract all data
   {
   query <- paste0("SELECT *
-FROM t1det9sp
+FROM t1det9sp_new
 WHERE region =","'",which.region,"'","AND geargrp=","'",which.gear,"'",
                   "AND datatype=","'",which.datatype,"'")
   }
@@ -23,7 +23,7 @@ WHERE region =","'",which.region,"'","AND geargrp=","'",which.gear,"'",
 else{
 query <- paste0("SELECT * 
 
-FROM t1det9sp
+FROM t1det9sp_new
 WHERE region =","'",which.region,"'","AND geargrp=","'",which.gear,"'",
                 "AND flag=","'",which.flag,"'","AND datatype=","'",which.datatype,"'")
 }
