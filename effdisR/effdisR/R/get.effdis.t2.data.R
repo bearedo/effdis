49ffae41142e7c0beee0a4cfa1b13a6a25de8da8
bearedo <doug.beare@gmail.com>
2015-11-18 08:37:20 +0000

    changes for the new data

--- effdisR/effdisR/R/get.effdis.t2.data.R
+++ effdisR/effdisR/R/get.effdis.t2.data.R
@@ -3,11 +3,11 @@ function(which.dsn="effdis-tuna-cc1",which.gear='LL',which.region='AT',which.fla
                                  which.dsettype='-w')
 {
 
- # which.gear <- "LL"
- # which.region <- "AT"
- # which.flag <- "EU.Portugal"
- # which.effort <- "NO.HOOKS"
- # which.dsettype <- 'nw'
+  which.gear <- "LL"
+  which.region <- "AT"
+  which.flag <- "EU.Portugal"
+  which.effort <- "NO.HOOKS"
+  which.dsettype <- 'nw'
   
   # Function to extract task 2 data from effdis cloud server ()
   
@@ -18,7 +18,7 @@ if(which.flag=='All') # If which.flag is 'All' extract all data
   query <- paste0("SELECT yearc AS year, trend, timeperiodid AS month, flagname, fleetcode,region, squaretypecode,
 geargrpcode,longitude,latitude, catchunit, dsettype, eff1, eff1type,
 alb,bft,bet,skj,yft,swo,bum,sai,whm,totsp9
-FROM t2ce
+FROM t2ce_new
 WHERE region =","'",which.region,"'","AND timeperiodid < 13 AND geargrpcode=","'",which.gear,"'",
                   "AND dsettype=","'",which.dsettype,"'")
   }
@@ -28,7 +28,7 @@ query <- paste0("SELECT yearc AS year, trend, timeperiodid AS month, flagname, f
 geargrpcode,longitude,latitude, catchunit, dsettype, eff1, eff1type,
 alb,bft,bet,skj,yft,swo,bum,sai,whm,totsp9
 
-FROM t2ce
+FROM t2ce_new
 WHERE region =","'",which.region,"'","AND timeperiodid < 13 AND geargrpcode=","'",which.gear,"'",
                 "AND flagname=","'",which.flag,"'","AND dsettype=","'",which.dsettype,"'")
 }
