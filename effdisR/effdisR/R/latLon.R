9ab6e2c52c06b2c932782ed15fb0294b0b28b070
Doug Beare <DBeare@iccat.local>
2015-12-03 13:36:09 +0100

    Convert longitudes and latitudes (due to L. Kell)

---
+++ effdisR/effdisR/R/latLon.R
@@ -0,0 +1,24 @@
+latLon<-function(x){
+  
+  x$lat<-x$lat + switch(x$square,
+                        "1x1"    = 0.5,
+                        "5x5"    = 2.5,
+                        "10x20"  = 5.0,
+                        "10x10"  = 5.0,
+                        "5x10"   = 2.5,
+                        "20x20"  = 10.0,
+                        "none"   = 0.0)
+  
+  x$lon<-x$lon + switch(x$square,
+                        "1x1"    =  0.5,
+                        "5x5"    =  2.5,
+                        "10x20"  = 10.0,
+                        "10x10"  =  5.0,
+                        "5x10"   =  5.0,
+                        "20x20"  = 10.0,
+                        "none"   =  0.0)
+  
+  x[x$quad %in% 3:4, "lon"]<--x[x$quad %in% 3:4, "lon"]
+  x[x$quad %in% 2:3, "lat"]<--x[x$quad %in% 2:3, "lat"]
+  
+  return(x)}
