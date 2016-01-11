1adee94b1bf8e718c1fc35838196bf7f6306e937
Doug Beare <DBeare@iccat.local>
2016-01-11 13:29:12 +0100

    get lon and lat too

--- effdisR/effdisR/R/get.effdis.t2.data.R
+++ effdisR/effdisR/R/get.effdis.t2.data.R
@@ -16,7 +16,7 @@ function(which.dsn="effdis-tuna-cc1",which.gear='LL',which.region='AT',which.fla
 if(which.flag=='All') # If which.flag is 'All' extract all data
   {
   query <- paste0("SELECT yearc AS year, trend, timeperiodid AS month, flagname, fleetcode,region, squaretypecode,quadid,
-geargrpcode,longitude,latitude, catchunit, dsettype, eff1, eff1type,
+geargrpcode,lon,lat,longitude,latitude, catchunit, dsettype, eff1, eff1type,
 alb,bft,bet,skj,yft,swo,bum,sai,whm,totsp9
 FROM t2ce_new
 WHERE region =","'",which.region,"'","AND timeperiodid < 13 AND geargrpcode=","'",which.gear,"'",
@@ -25,7 +25,7 @@ WHERE region =","'",which.region,"'","AND timeperiodid < 13 AND geargrpcode=","'
 
 else{
 query <- paste0("SELECT yearc AS year, trend, timeperiodid AS month, flagname, fleetcode,region,squaretypecode,quadid,
-geargrpcode,longitude,latitude, catchunit, dsettype, eff1, eff1type,
+geargrpcode,lon,lat,longitude,latitude, catchunit, dsettype, eff1, eff1type,
 alb,bft,bet,skj,yft,swo,bum,sai,whm,totsp9
 
 FROM t2ce_new
