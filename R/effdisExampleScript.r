1edc5be9721b05d03ffc8519bb2734ffb9bce036
Doug Beare <DBeare@iccat.local>
2016-01-12 18:07:29 +0100

    tinkering

--- R/effdisExampleScript.r
+++ R/effdisExampleScript.r
@@ -37,6 +37,22 @@ psw  <- get.effdis.t2.data(which.dsn='effdis-tuna-cc1',which.gear='PS',which.fla
 
 ps1 <- rbind(psn,psnw,psw)
 
+# Double-check that lat and long conversions have been done properly #
+
+df <- data.frame(quad=ps1$quadid,lat=ps1$lat,lon=ps1$lon,square=ps1$squaretypecode) # simplify data.frame
+df$square <- as.character(df$square) # function requires input to be 'character'
+
+hl <- length(df[,1])
+ndf <- data.frame(quad=rep(NA,hl),lat=rep(NA,hl),lon=rep(NA,hl),square=rep(NA,hl))
+for(i in 1:hl){
+  ndf[i,] <- latLon(df[i,]) # calculate the central lat long of each grid looping thro' each row
+}
+
+plot(ndf$lon,ndf$lat,pch=".")
+
+ps1$longitude <- ndf$lon # add on lat and long to main database
+ps1$latitude <- ndf$lat
+
 table(ps1$squaretypecode,ps1$flagname)
 
 t2 <- tapply(ps1$totsp9,list(ps1$flagname,ps1$year),sum,na.rm=T)
@@ -50,14 +66,14 @@ ps1<-ps1[ps1$squaretypecode == '1x1',]
 library(reshape2)
 pslf <- convert2long.format.t2(input =ps1)
 
-#bm <- model.nos.kgs.r(input=pslf,which.gear='PS')
+#bm <- model.nos.kgs(input=pslf,which.gear='PS')
 
 #pslf <- kgs.from.nos.r(pslf) # Not relevant for PS 
 
 table(pslf$eff1type)
 
-# D.AT SEA    D.FISH  NO.BOATS    -none- FISH.HOUR HOURS.SEA   NO.SETS  NO.TRIPS  SUC.D.FI  SUC.SETS 
-# 2457    643176       297      2376    925920         0     28008       423         9        72 
+# -none-  D.AT SEA    D.FISH FISH.HOUR   NO.SETS  NO.TRIPS  SUC.D.FI  SUC.SETS 
+# 9729      1881    237051    531621     24642       369        27       126 
 
 pslf <- pslf[pslf$eff1type %in% c('D.FISH','FISH.HOUR'),]
 pslf$eff1[pslf$eff1type == 'D.FISH'] <- pslf$eff1[pslf$eff1type == 'D.FISH']*24
@@ -67,119 +83,167 @@ w0 <- (1:length(pslf$year))[pslf$catchunit == 'kg']
 
 round(tapply(pslf$measured_catch[w0],list(pslf$flagname[w0],pslf$species[w0]),sum,na.rm=T))/1000
 
-#                alb       bft        bet         skj         yft   swo   bum  sai whm
-# U.S.A.         25.825 31397.963   2360.765  128429.735  103531.315 0.000 0.000 0.00   0
-# EU.España    4521.090   903.900 168162.540 1140806.620 1216948.670 0.000 0.000 0.00   0
-# Other        4299.320  3451.287 274045.180 1279444.048 1629395.695 0.000 0.000 0.00   0
-# Belize         96.125     0.000      0.000       0.000       0.000 0.000 0.000 0.00   0
-# Brasil          0.000     0.000      0.000     808.425     504.000 0.000 0.000 0.00   0
-# EU.Malta           NA        NA         NA          NA          NA    NA    NA   NA  NA
-# EU.Portugal     7.120     0.865      0.000      14.069       0.437 0.026 0.797 0.31   0
-# Japan          25.100     0.000    182.700   23822.400   18998.900 0.000 0.000 0.00   0
-# Panama         38.530     0.000  16523.880   76855.650   43251.450 0.000 0.000 0.00   0
-# South Africa    0.000     0.000      0.000     122.000     151.000 0.000 0.000 0.00   0
-# U.S.S.R.           NA        NA         NA          NA          NA    NA    NA   NA  NA
-# Venezuela    2941.085     0.000   6041.951  120893.778  194309.645 0.000 0.000 0.00   0
-            
-alb.ps <- fit2stageGAMtoCatch.r(input=pslf,which.flag='EU.España',which.species='alb',start.year=1970,end.year=2015)
-#bft.ps <- fit2stageGAMtoCatch.r(input=pslf,which.flag='EU.España',which.species='bft',start.year=1970,end.year=2015,kk=3)
-bet.ps <- fit2stageGAMtoCatch.r(input=pslf,which.flag='EU.España',which.species='bet',start.year=1970,end.year=2015)
-skj.ps <- fit2stageGAMtoCatch.r(input=pslf,which.flag='EU.España',which.species='skj',start.year=1970,end.year=2015)
-yft.ps <- fit2stageGAMtoCatch.r(input=pslf,which.flag='EU.España',which.species='yft',start.year=1970,end.year=2015)
+pslf <- pslf[pslf$catchunit == "kg",]
+pslf$species <- as.character(pslf$species)
 
-# Do we just assume here that once we've modeled NO.HOOKS as a function of time we can use that sensibly ?
+dim(pslf) #=  768672 (2015)
 
-emod.ps <- fitGAMtoEffort.r(input=pslf,which.flag='EU.España',which.effort='FISH.HOUR',start.year=1990,end.year=2015)
+us <- sort(as.character(unique(pslf$species)))
+uf <- sort(as.character(unique(pslf$flagname)))
 
-# Create grids and predict over them ###
+ uf
 
-alb.aa.ps <- predict.effdis.t2.data.r(cmod=alb.ps, effmod=emod.ps,grid.res=1,start.year=1970,end.year=2015,which.flag='EU.España',which.gear = 'PS')
-#bft.aa.ps <- predict.effdis.t2.data.r(cmod=bft.ps, effmod=emod.ps,grid.res=5,start.year=1970,end.year=2015,which.flag='EU.España',which.gear = 'PS')
-bet.aa.ps <- predict.effdis.t2.data.r(cmod=bet.ps, effmod=emod.ps,grid.res=1,start.year=1970,end.year=2015,which.flag='EU.España',which.gear = 'PS')
-skj.aa.ps <- predict.effdis.t2.data.r(cmod=skj.ps, effmod=emod.ps,grid.res=1,start.year=1970,end.year=2015,which.flag='EU.España',which.gear = 'PS')
-yft.aa.ps <- predict.effdis.t2.data.r(cmod=yft.ps, effmod=emod.ps,grid.res=1,start.year=1970,end.year=2015,which.flag='EU.España',which.gear = 'PS')
+#  [1] "Belize"                "Brazil"                "Canada"                "Cape Verde"            "Congo"                 "Côte D'Ivoire"        
+#  [7] "Curaçao"               "EU.España"             "EU.France"             "EU.Portugal"           "Ghana"                 "Ghana (ICCAT program)"
+#  [13] "Guatemala"             "Guinée Rep."           "Japan"                 "Mixed flags (FIS)"     "NEI (ETRO)"            "Panama"               
+#  [19] "Russian Federation"    "U.S.A."                "Venezuela"            
+#  
+ 
+ 
+  #setwd('/home/doug/effdis/data')
+setwd("c:/Users/DBeare/effdis/effdis/data/effdis-estimates")
+
+#Flags to add to "others"- 
+
+oth <-c("Argentina", "Angola", "Barbados", "Cape Verde","China (ICCAT program)", "Côte D'Ivoire","EU.France",
+        "Chinese Taipei (foreign obs.)", "Cuba (ICCAT program)" ,"Dominica","EU.United.Kingdom","Faroe Islands",
+        "FR.St Pierre et Miquelon","Grenada","Guinea Ecuatorial","Honduras","Iceland","Japan (foreign obs.)",
+        "Maroc","Mexico","Panama","Philippines","Sierra Leone","Trinidad and Tobago","UK.Sta Helena","UK.Turks and Caicos","Uruguay","U.S.S.R.")
+
+idx <- (1:length(lllf[,1]))[lllf$flagname %in% oth]
+
+lllf$flagname[idx] <- "Other"
 
 
-# Plot data 
-par(mfrow=c(3,3))
-plot.mods.r(input=alb.aa.ps,cmod=alb.ps,what.year = 1992,what.month=1,what.value = 'catch',grid.res=1)
-#plot.mods.r(input=bft.aa,cmod=bft,what.year = 1995,what.month=1,what.value = 'prob',grid.res=5)
-plot.mods.r(input=bet.aa.ps,cmod=bet.ps,what.year = 1994,what.month=3,what.value = 'catch',grid.res=1,plot.samples.only=T)
-plot.mods.r(input=skj.aa.ps,cmod=skj.ps,what.year = 1995,what.month=1,what.value = 'catch',grid.res=1)
-par(mfrow=c(3,4))
-for(i in 1:12)
+#Japan
+
+emod.jap <- fitGAMtoEffort(input=pslf,which.flag='Japan',which.effort='FISH.HOUR',start.year=1980,end.year=2015,kk=6)
+
+mod.jap <- as.list(1:9)
+for(i in 1:9)
 {
-plot.mods.r(input=yft.aa.ps,cmod=yft.ps,what.year = 1995,what.month=i,what.value = 'prob',grid.res=1)
+  sp <- us[i]
+  mod.jap[[i]] <- fit2stageGAMtoCatch(input=pslf,which.flag='Japan',which.species=sp,start.year=1980,end.year=2015,kk=6)
+  print(sp)
 }
 
-plot(alb.aa$longitude,alb.aa$latitude)
-points(alb$pmod.data$longitude,alb$pmod.data$latitude,col='red')
 
-#Get Task 1 data
-
-ps.t1 <- get.effdis.t1.data.r(which.dsn='effdis-local',which.gear = 'PS',which.region='AT',which.flag='EU.España')
-#ll.t1 <- get.effdis.t1.data.r(which.dsn='effdis-local',which.gear = 'LL',which.region='AT',which.flag='All')
-
-w1 <- (1:length(ps.t1[,1]))[ps.t1$yearc == 1990]
-tapply(ps.t1$qty_t[w1],list(ps.t1$flag[w1],ps.t1$species[w1]),sum,na.rm=T)
-
-# See task 1 totals by species
-
-t1x <- aggregate(list(qty_t=ps.t1$qty_t),list(year=ps.t1$yearc,species = ps.t1$species),sum,na.rm=T)
-t1x[t1x$species == 'bet',]
-
-#Bind up estimates for 4 different species
-
-big <- rbind(alb.aa.ps,bet.aa.ps,skj.aa.ps,yft.aa.ps)
-
-big<- big[big$which.ocean == "atl",]
-
-big$catch[big$observation == FALSE] <- NA
-big$prob[big$observation == FALSE] <- NA
-big$measured_catch[big$observation == FALSE] <- NA
-big$eff[big$observation == FALSE]  <- NA
-
-#xxx <- aggregate(list(measured_catch=big$measured_catch,catch=big$catch,eff=big$eff), 
-#by=list(year=big$year,species=big$species),sum,na.rm=T)
-
-#xxx.90 <- xxx[xxx$year == 1990 & xxx$species == 'bft',]
-#sum(xxx.90$catch)/1000
+for(i in 1:9)
+{
+  if(mod.jap[[i]]=='Insufficient data to support model')
+  {print('no model')
+  }
+  else{
+    aa <- predict.effdis.t2.data(cmod=mod.jap[[i]],which.gear="PS", effmod=emod.jap,grid.res=1,start.year=1990,end.year=2015,which.flag='Japan')
+    rm(aa)
+    gc(reset=T)
+  }
+}
 
 
-#Convert catch to tonnes from kgs
+aa <- predict.effdis.t2.data(cmod=mod.jap[[9]],which.gear="PS", effmod=emod.jap,grid.res=1,start.year=1990,end.year=2015,which.flag='Japan',observations=F)
+plot.mods(input=aa,cmod=mod.jap[[9]],which.year=1995,which.month=1,grid.res=7,which.value = 'prob',which.gear='PS',plot.samples.only=F)
+    
 
-big$catch <- big$catch/1000
-big$measured_catch <- big$measured_catch/1000
-big$eff <- big$eff/4 # Effort is the same for each species so divide by 4
+#Cape Verde
+
+ emod<- pslf[pslf$flagname == "Cape Verde",]
+
+emod.cv <- fitGAMtoEffort(input=pslf,which.flag='Cape Verde',which.effort='FISH.HOUR',start.year=1980,end.year=2015,kk=3)
+
+mod.cv <- as.list(1:9)
+for(i in 1:9)
+{
+  sp <- us[i]
+  mod.cv[[i]] <- fit2stageGAMtoCatch(input=pslf,which.flag='Cape Verde',which.species=sp,start.year=1980,end.year=2015,kk=3)
+  print(sp)
+}
+
+for(i in 1:9)
+{
+  if(mod.cv[[i]]=='Insufficient data to support model')
+  {print('no model')
+  }
+  else{
+    aa <- predict.effdis.t2.data(cmod=mod.cv[[i]],which.gear="PS", effmod=emod.cv,grid.res=1,start.year=1980,end.year=2015,which.flag='Cape Verde')
+    rm(aa)
+    gc(reset=T)
+  }
+}
+
+#Curaçao
+
+emod.cur <- fitGAMtoEffort(input=pslf,which.flag="Curaçao",which.effort='FISH.HOUR',start.year=1980,end.year=2015,kk=3)
+
+mod.cur <- as.list(1:9)
+for(i in 1:9)
+{
+  sp <- us[i]
+  mod.cur[[i]] <- fit2stageGAMtoCatch(input=pslf,which.flag="Curaçao",which.species=sp,start.year=1980,end.year=2015,kk=3)
+  print(sp)
+}
+
+for(i in 1:9)
+{
+  if(mod.cur[[i]]=='Insufficient data to support model')
+  {print('no model')
+  }
+  else{
+    aa <- predict.effdis.t2.data(cmod=mod.cur[[i]],which.gear="PS", effmod=emod.cur,grid.res=1,start.year=1980,end.year=2015,which.flag="Curaçao")
+    rm(aa)
+    gc(reset=T)
+  }
+}
+
+#EU.Espana
+
+emod.esp <- fitGAMtoEffort(input=pslf,which.flag="EU.España",which.effort='FISH.HOUR',start.year=1980,end.year=2015,kk=9)
+
+mod.esp <- as.list(1:9)
+for(i in 1:9)
+{
+  sp <- us[i]
+  mod.esp[[i]] <- fit2stageGAMtoCatch(input=pslf,which.flag="EU.España",which.species=sp,start.year=1980,end.year=2015,kk=9)
+  print(sp)
+}
+
+for(i in 1:9)
+{
+  if(mod.esp[[i]]=='Insufficient data to support model')
+  {print('no model')
+  }
+  else{
+    aa <- predict.effdis.t2.data(cmod=mod.esp[[i]],which.gear="PS", effmod=emod.esp,grid.res=1,start.year=1980,end.year=2015,which.flag="EU.España")
+    rm(aa)
+    gc(reset=T)
+  }
+}
 
 
-big1 <- aggregate(list(measured_catch=big$measured_catch,catch=big$catch,eff=big$eff), 
-                  by=list(year=big$year),sum,na.rm=T)
+#EU.France
 
-big1$cpue <- big1$catch/big1$eff
+emod.fra <- fitGAMtoEffort(input=pslf,which.flag="EU.France",which.effort='FISH.HOUR',start.year=1980,end.year=2015,kk=9)
 
+mod.fra <- as.list(1:9)
+for(i in 1:9)
+{
+  sp <- us[i]
+  mod.fra[[i]] <- fit2stageGAMtoCatch(input=pslf,which.flag="EU.France",which.species=sp,start.year=1980,end.year=2015,kk=9)
+  print(sp)
+}
 
-# Aggregate estimates from Task 1
+for(i in 1:9)
+{
+  if(mod.fra[[i]]=='Insufficient data to support model')
+  {print('no model')
+  }
+  else{
+    aa <- predict.effdis.t2.data(cmod=mod.esp[[i]],which.gear="PS", effmod=emod.esp,grid.res=1,start.year=1980,end.year=2015,which.flag="EU.España")
+    rm(aa)
+    gc(reset=T)
+  }
+}
 
-sum.t1 <- aggregate(list(qty_t=ll.t1$qty_t),list(year=ll.t1$yearc),sum,na.rm=T)
-
-# Merge t1 and t2
-
-big2 <- merge(big1,sum.t1)
-
-big2$cpue <- big2$measured_catch/big2$eff
-
-
-plot(big2$year,big2$catch,ylim=c(range(big2$catch,big2$qty_t)))
-lines(big2$year,big2$qty_t)
-
-
-big2$new.effort <- big2$qty_t/big2$cpue
-
-write.table(big2,'/home/doug/effdis/data/effdis-estimate.csv',sep=',',row.names=F)
-
-par(mfrow=c(1,1))
-plot(big2$year,big2$new.effort/1000000,xlim=c(1970,2010),type='l')
 
 ##########################################
 ######## Longliners  #####################
@@ -195,6 +259,11 @@ llw  <- get.effdis.t2.data(which.dsn='effdis-tuna-cc1',which.gear='LL',which.fla
 ll1 <- rbind(lln,llnw,llw)
 
 
+
+
+
+
+
 # Set up data for modeled estimate (have to have some spatial detail)
 ll1 <- convert.grid.res(ll1) # Some data, eg. Portugal are supplied at 1x1 but for the modeling we need to be consistent.
 data("seas")
@@ -205,6 +274,7 @@ ll1 <- ll1[ll1$which.ocean == 'atl',]
 ll1 <- ll1[ll1$squaretypecode == '5x5',]
 lllf <- convert2long.format.t2(input =ll1) # NB I do this not because it matters for the modeling but because I need to know where the data were and want to do predictions on an ordely grid?
 lllf<-prepare.effdis.data(input=lllf)
+
 bm <- model.nos.kgs(input=lllf,which.gear='LL')
 lllf <- kgs.from.nos(lllf) # for those fleets that supply only number
 #lllf <- lllf[lllf$eff1type=='NO.HOOKS',] # Quite a no of eff1type = '-none-' but they are important e.g. Spanish LL so leave them in here, model them and use the model to predict effort at the places where it is none or something else.
