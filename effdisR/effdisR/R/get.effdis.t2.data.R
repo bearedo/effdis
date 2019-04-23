a02984da03e2725bfb5bf5ccdda24dba0d2e1156
Doug Beare <doug.beare@gmail.com>
2019-04-23 10:45:20 +0200

    change 2017 to 2019

--- effdisR/effdisR/R/get.effdis.t2.data.R
+++ effdisR/effdisR/R/get.effdis.t2.data.R
@@ -16,21 +16,21 @@ function(which.dsn="effdis-tuna-cc1",which.gear='LL',which.region='AT',which.fla
 if(which.flag=='All') # If which.flag is 'All' extract all data
   {
   query <- paste0("SELECT yearc AS year, trend, timeperiodid AS month, flagname, fleetcode,region, squaretypecode,quadid,
-geargrpcode,lon,lat,longitude,latitude, catchunit, dsettypeid, eff1, eff1type,
+geargrpcode,lon,lat,longitude,latitude, catchunit, dsettype, eff1, eff1type,
 alb,bft,bet,skj,yft,swo,bum,sai,whm,totsp9
-FROM t2ce_2017
+FROM t2ce_2019
 WHERE region =","'",which.region,"'","AND timeperiodid < 13 AND geargrpcode=","'",which.gear,"'",
-                  "AND dsettypeid=","'",which.dsettype,"'")
+                  "AND dsettype=","'",which.dsettype,"'")
   }
 
 else{
 query <- paste0("SELECT yearc AS year, trend, timeperiodid AS month, flagname, fleetcode,region,squaretypecode,quadid,
-geargrpcode,lon,lat,longitude,latitude, catchunit, dsettypeid, eff1, eff1type,
+geargrpcode,lon,lat,longitude,latitude, catchunit, dsettype, eff1, eff1type,
 alb,bft,bet,skj,yft,swo,bum,sai,whm,totsp9
 
-FROM t2ce_2017
+FROM t2ce_2019
 WHERE region =","'",which.region,"'","AND timeperiodid < 13 AND geargrpcode=","'",which.gear,"'",
-                "AND flagname=","'",which.flag,"'","AND dsettypeid=","'",which.dsettype,"'")
+                "AND flagname=","'",which.flag,"'","AND dsettype=","'",which.dsettype,"'")
 }
 
   out <- sqlQuery(chan,query)
